6d4fd9ac418aa0bb459cb3decfe689bbde998232
Piotr Jura <jura.piotrek@gmail.com>
2023-07-30 20:02:15 +0200

    0022-route-params

---
+++ portfolio/pages/blog/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    We are inside a blog!
+  </div>
+  <div>
+    Post id is {{ route.params.id }}
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+// console.log(route)
+</script>
---
+++ portfolio/pages/blog/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>All blog posts list!</div>
+</template>
