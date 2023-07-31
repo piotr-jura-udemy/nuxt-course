1a755f358f4e7f5f89246acc1367b65bc0a3187a
Piotr Jura <jura.piotrek@gmail.com>
2023-07-31 19:49:44 +0200

    0023-layouts

--- portfolio/app.vue
+++ portfolio/app.vue
@@ -1,15 +1,5 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Main</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-    </ul>
-  </nav>
-  <div>
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
---
+++ portfolio/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Main</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <slot />
+  </div>
+</template>
