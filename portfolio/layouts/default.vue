00491e6d2dce277138198dd06accfc9450e200fb
Piotr Jura <jura.piotrek@gmail.com>
2023-08-02 19:34:01 +0200

    0026-styling-the-app-with-tailwind

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Main</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Piotr Jura</NuxtLink>
+      </div>
+
+      <nav class="font-mono">
+        <ul class="flex space-x-4">
+          <li>
+            <NuxtLink to="/">Main</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
