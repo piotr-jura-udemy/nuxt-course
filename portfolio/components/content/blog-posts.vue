9847c8d185fb4a39adcf1cfa5db10b7b7c4b46f0
Piotr Jura <jura.piotrek@gmail.com>
2023-08-12 22:22:47 +0200

    0040-mdc-syntax

--- portfolio/components/content/blog-posts.vue
+++ portfolio/components/content/blog-posts.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="not-prose">
     <ul>
       <li v-for="post in posts" :key="post._path">
         <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
@@ -11,7 +11,10 @@
 <script setup>
 const { data: posts } = await useAsyncData(
   'blog-list',
-  () => queryContent('/blog').only(['_path', 'title']).find()
+  () => queryContent('/blog')
+    .where({ _path: { $ne: '/blog' } })
+    .only(['_path', 'title'])
+    .find()
 )
 console.log(posts)
 </script>
---
+++ portfolio/content/blog/index.md
@@ -0,0 +1,8 @@
+---
+description: 'List of all my recent blog posts'
+---
+# Blog Posts
+
+This is the list of my recent blog posts
+
+::blog-posts
