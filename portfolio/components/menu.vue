95059ed569dafc6e54ba39d745f145c5861af673
Piotr Jura <jura.piotrek@gmail.com>
2023-08-12 15:03:58 +0200

    0039-post-list-querying

--- portfolio/components/menu.vue
+++ portfolio/components/menu.vue
@@ -10,6 +10,9 @@
       <li>
         <NuxtLink to="/projects" class="link">Projects</NuxtLink>
       </li>
+      <li>
+        <NuxtLink to="/blog" class="link">Blog</NuxtLink>
+      </li>
     </ul>
   </nav>
 </template>
--- portfolio/pages/blog/index.vue
+++ portfolio/pages/blog/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>All blog posts list!</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+const { data: posts } = await useAsyncData(
+  'blog-list',
+  () => queryContent('/blog').only(['_path', 'title']).find()
+)
+console.log(posts)
+</script>
