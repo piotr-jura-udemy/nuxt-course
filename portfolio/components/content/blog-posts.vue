2d00e6d116e70d8a0f9c35fbaed597244351ad76
Piotr Jura <jura.piotrek@gmail.com>
2023-08-13 14:15:48 +0200

    0042-blog-posts-redesign-logic

--- portfolio/components/content/blog-posts.vue
+++ portfolio/components/content/blog-posts.vue
@@ -8,7 +8,9 @@
     <ul>
       <li v-for="post in posts" :key="post._path">
         <NuxtLink :to="post._path" class="column hover:bg-gray-100 dark:hover:bg-gray-800">
-          <div class="text-gray-500">2023</div>
+          <div
+            :class="{ 'text-white dark:text-gray-900': !post.displayYear, 'text-gray-400 dark:text-gray-500': post.displayYear }">
+            {{ post.year }}</div>
           <div>{{ post.title }}</div>
         </NuxtLink>
       </li>
@@ -17,13 +19,35 @@
 </template>
 
 <script setup>
-const { data: posts } = await useAsyncData(
+const { data } = await useAsyncData(
   'blog-list',
   () => queryContent('/blog')
     .where({ _path: { $ne: '/blog' } })
     .only(['_path', 'title', 'publishedAt'])
+    .sort({ publishedAt: -1 })
     .find()
 )
+
+const posts = computed(() => {
+  if (!data.value) {
+    return []
+  }
+
+  const result = []
+  let lastYear = null
+
+  for (const post of data.value) {
+    const year = new Date(post.publishedAt).getFullYear()
+    const displayYear = year !== lastYear
+    post.displayYear = displayYear
+    post.year = year
+    result.push(post)
+    lastYear = year
+  }
+
+  return result
+})
+
 console.log(posts)
 </script>
 
