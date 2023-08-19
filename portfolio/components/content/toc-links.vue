e53b4fa965ffb43b6e0846d75021d5ba06c866f6
Piotr Jura <jura.piotrek@gmail.com>
2023-08-19 16:58:09 +0200

    0047-toc-active-section

--- portfolio/components/content/toc-links.vue
+++ portfolio/components/content/toc-links.vue
@@ -1,10 +1,11 @@
 <template>
   <ul>
     <li v-for="link in links" :key="link.id">
-      <NuxtLink :to="{ path: route.path, hash: `#${link.id}` }" :class="{ 'ml-4': level }">
+      <NuxtLink :to="{ path: route.path, hash: `#${link.id}` }"
+        :class="{ 'ml-4': level, 'text-green-600 dark:text-green-400': activeId === link.id }">
         {{ link.text }}
       </NuxtLink>
-      <TocLinks :links="link.children" :level="level + 1" />
+      <TocLinks :links="link.children" :level="level + 1" :active-id="activeId" />
     </li>
   </ul>
 </template>
@@ -16,6 +17,10 @@ defineProps({
   level: {
     type: Number,
     default: 0
+  },
+  activeId: {
+    type: String,
+    default: null
   }
 })
 </script>
--- portfolio/pages/blog/[...slug].vue
+++ portfolio/pages/blog/[...slug].vue
@@ -12,18 +12,42 @@
               Table of Contents
             </div>
             <nav>
-              <TocLinks :links="doc.body.toc.links" />
+              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
             </nav>
           </aside>
         </div>
       </div>
-
-
     </ContentDoc>
   </article>
 </template>
 
 <script setup>
-const route = useRoute()
-console.log(route.path)
+const activeId = ref(null)
+
+onMounted(() => {
+  const callback = (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id
+        break;
+      }
+    }
+  }
+
+  const observer = new IntersectionObserver(callback, {
+    root: null,
+    threshold: 0.5
+  })
+  const elements = document.querySelectorAll('h2, h3')
+
+  for (const element of elements) {
+    observer.observe(element)
+  }
+
+  onBeforeUnmount(() => {
+    for (const element of elements) {
+      observer.unobserve(element)
+    }
+  })
+})
 </script>
