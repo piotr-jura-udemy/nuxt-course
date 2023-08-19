da0664232858f21671fc5eef7fc4b4976507add0
Piotr Jura <jura.piotrek@gmail.com>
2023-08-19 19:02:17 +0200

    0048-error-pages

---
+++ portfolio/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout>
+    <section>
+      <h1 class="text-4xl mb-4">
+        Opps {{ error.statusCode }}
+      </h1>
+
+      <h2 class="text-xl text-gray-500 dark:text-gray-400">
+        {{ error.statusMessage }}
+      </h2>
+    </section>
+  </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({ error: Object })
+</script>
--- portfolio/pages/blog/[...slug].vue
+++ portfolio/pages/blog/[...slug].vue
@@ -1,22 +1,28 @@
 <template>
   <article
     class="prose dark:prose-invert max-w-none prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300">
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>This blog post could not be found.</p>
+      </template>
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="col-span-2 not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">
+                Table of Contents
+              </div>
+              <nav>
+                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div class="col-span-2 not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">
-              Table of Contents
-            </div>
-            <nav>
-              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-            </nav>
-          </aside>
-        </div>
-      </div>
+      </template>
     </ContentDoc>
   </article>
 </template>
