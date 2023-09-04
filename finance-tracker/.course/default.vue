f7f88a9fa1a8d300b9bf3ca08effaa5673bbbe1d
Piotr Jura <jura.piotrek@gmail.com>
2023-09-04 17:40:12 +0200

    0101-nuxtui-layout-header

---
+++ finance-tracker/.course/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container mx-auto max-w-4xl">
+    <main class="my-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+// Link to Google Fonts
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
+
+<style>
+/* Set the font family for the entire site */
+body {
+  font-family: 'Inter';
+}
+/* Set the background color for the entire site */
+body {
+  @apply dark:bg-gray-900 bg-white
+}
+</style>
---
+++ finance-tracker/components/app-header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold">
+      Finance Tracker
+    </NuxtLink>
+    <div>
+      <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+    </div>
+  </header>
+</template>
