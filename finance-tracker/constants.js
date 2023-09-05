6d791b6bf29a3f865343064afe43dab0fce2081f
Piotr Jura <jura.piotrek@gmail.com>
2023-09-05 17:10:11 +0200

    0102-summary-and-period-selection

---
+++ finance-tracker/constants.js
@@ -0,0 +1,1 @@
+export const transactionViewOptions = ['Yearly', 'Monthly', 'Daily']
--- finance-tracker/pages/index.vue
+++ finance-tracker/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Index page!</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">
+      Summary
+    </h1>
+    <div>
+      <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+    </div>
+  </section>
 </template>
+
+<script setup>
+import { transactionViewOptions } from '~/constants'
+const selectedView = ref(transactionViewOptions[1])
+</script>
