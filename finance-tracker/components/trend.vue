76454bde08a366d5ae64dddc3db0084d20da50a2
Piotr Jura <jura.piotrek@gmail.com>
2023-09-05 18:06:32 +0200

    0103-trend-icons-and-loader

---
+++ finance-tracker/components/trend.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="font-bold" :class="[color]">{{ title }}</div>
+
+    <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+      <USkeleton class="h-8 w-full" v-if="loading" />
+      <div v-else>{{ amount }}</div>
+    </div>
+
+    <div>
+      <USkeleton class="h-6 w-full" v-if="loading" />
+      <div v-else class="flex space-x-1 items-center text-sm">
+        <UIcon name="i-heroicons-arrow-trending-up" class="w-6 h-6" :class="[color]" />
+        <div class="text-gray-500 dark:text-gray-400">
+          30% vs last period
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  amount: Number,
+  lastAmount: Number,
+  color: String,
+  loading: Boolean
+})
+</script>
+
+<style scoped>
+.green {
+  @apply text-green-600 dark:text-green-400
+}
+.red {
+  @apply text-red-600 dark:text-red-400
+}
+</style>
--- finance-tracker/pages/index.vue
+++ finance-tracker/pages/index.vue
@@ -7,6 +7,13 @@
       <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
     </div>
   </section>
+
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+    <Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+    <Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="true" />
+  </section>
 </template>
 
 <script setup>
