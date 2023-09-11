05604ad67a83347b3f3f3e61ffce38318b47c854
Piotr Jura <jura.piotrek@gmail.com>
2023-09-11 14:35:54 +0200

    0108-nuxt-and-supabase

--- finance-tracker/nuxt.config.ts
+++ finance-tracker/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/ui']
+  modules: ['@nuxthq/ui', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+  }
 })
