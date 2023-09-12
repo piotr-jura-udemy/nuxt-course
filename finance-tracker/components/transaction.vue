297885a4d08ad18c3acdebf0c29470006ed32415
Piotr Jura <jura.piotrek@gmail.com>
2023-09-12 18:32:52 +0200

    0111-income-or-expense

--- finance-tracker/components/transaction.vue
+++ finance-tracker/components/transaction.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100">
     <div class="flex items-center justify-between">
       <div class="flex items-center space-x-1">
-        <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
+        <UIcon :name="icon" :class="[iconColor]" />
         <div>{{ transaction.description }}</div>
       </div>
 
@@ -26,6 +26,15 @@
 const props = defineProps({
   transaction: Object
 })
+
+const isIncome = computed(() => props.transaction.type === 'Income')
+const icon = computed(
+  () => isIncome.value ? 'i-heroicons-arrow-up-right' : 'i-heroicons-arrow-down-left'
+)
+const iconColor = computed(
+  () => isIncome.value ? 'text-green-600' : 'text-red-600'
+)
+
 const { currency } = useCurrency(props.transaction.amount)
 const items = [
   [
