55cb969478a62e74dafaace8fb28a2dcbccae1d8
Piotr Jura <jura.piotrek@gmail.com>
2023-09-13 18:52:54 +0200

    0113-daily-summary-of-transactions

---
+++ finance-tracker/components/daily-transaction-summary.vue
@@ -0,0 +1,39 @@
+
+
+
+<template>
+  <div
+    class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-500 dark:text-gray-400 font-bold">
+    <div class="flex items-center justify-between">
+      {{ date }}
+    </div>
+
+    <div class="flex items-center justify-end mr-10">
+      {{ currency }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  date: String,
+  transactions: Array
+})
+
+const sum = computed(() => {
+  let sum = 0
+
+  for (const transaction of props.transactions) {
+    if (transaction.type === 'Income') {
+      sum += transaction.amount
+    } else {
+      sum -= transaction.amount
+    }
+  }
+
+  return sum
+})
+
+const { currency } = useCurrency(sum)
+</script>
+
--- finance-tracker/pages/index.vue
+++ finance-tracker/pages/index.vue
@@ -16,7 +16,10 @@
   </section>
 
   <section>
-    <Transaction v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
+    <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
+      <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
+      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" />
+    </div>
   </section>
 </template>
 
