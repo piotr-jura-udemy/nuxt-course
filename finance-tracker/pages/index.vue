ed45a2f65da2e1f88c376505746f4902e72bcc0b
Piotr Jura <jura.piotrek@gmail.com>
2023-09-14 19:20:21 +0200

    0116-transaction-totals

--- finance-tracker/pages/index.vue
+++ finance-tracker/pages/index.vue
@@ -9,10 +9,22 @@
   </section>
 
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="isLoading" />
+    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="4100" :loading="isLoading" />
+    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="3800" :loading="isLoading" />
     <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
     <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="isLoading" />
+  </section>
+
+  <section class="flex justify-between mb-10">
+    <div>
+      <h2 class="text-2xl font-extrabold">Transactions</h2>
+      <div class="text-gray-500 dark:text-gray-400">
+        You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this period
+      </div>
+    </div>
+    <div>
+      <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" />
+    </div>
   </section>
 
   <section v-if="!isLoading">
@@ -34,6 +46,23 @@ const supabase = useSupabaseClient()
 const selectedView = ref(transactionViewOptions[1])
 const transactions = ref([])
 const isLoading = ref(false)
+
+const income = computed(
+  () => transactions.value.filter(t => t.type === 'Income')
+)
+const expense = computed(
+  () => transactions.value.filter(t => t.type === 'Expense')
+)
+
+const incomeCount = computed(() => income.value.length)
+const expenseCount = computed(() => expense.value.length)
+
+const incomeTotal = computed(
+  () => income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
+const expenseTotal = computed(
+  () => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
 
 const fetchTransactions = async () => {
   isLoading.value = true
