2fd9e7e3c676ffd189d964b000ab093d3be93fe3
Piotr Jura <jura.piotrek@gmail.com>
2023-09-14 16:42:55 +0200

    0114-deleting-data-and-toast

--- finance-tracker/components/daily-transaction-summary.vue
+++ finance-tracker/components/daily-transaction-summary.vue
@@ -1,6 +1,3 @@
-
-
-
 <template>
   <div
     class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-500 dark:text-gray-400 font-bold">
--- finance-tracker/components/transaction.vue
+++ finance-tracker/components/transaction.vue
@@ -15,7 +15,7 @@
       <div>{{ currency }}</div>
       <div>
         <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-          <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" />
+          <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" :loading="isLoading" />
         </UDropdown>
       </div>
     </div>
@@ -36,6 +36,33 @@ const iconColor = computed(
 )
 
 const { currency } = useCurrency(props.transaction.amount)
+
+const isLoading = ref(false)
+const toast = useToast()
+const supabase = useSupabaseClient()
+
+const deleteTransaction = async () => {
+  isLoading.value = true
+
+  try {
+    await supabase.from('transactions')
+      .delete()
+      .eq('id', props.transaction.id)
+    toast.add({
+      title: 'Transaction deleted',
+      icon: 'i-heroicons-check-circle',
+    })
+    emit('deleted', props.transaction.id)
+  } catch (error) {
+    toast.add({
+      title: 'Transaction deleted',
+      icon: 'i-heroicons-exclamation-circle',
+    })
+  } finally {
+    isLoading.value = false
+  }
+}
+
 const items = [
   [
     {
@@ -46,7 +73,7 @@ const items = [
     {
       label: 'Delete',
       icon: 'i-heroicons-trash-20-solid',
-      click: () => console.log('Delete')
+      click: deleteTransaction
     }
   ]
 ]
