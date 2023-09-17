4b5aa68ca78e38e37224d094ce8b1e9892d0214e
Piotr Jura <jura.piotrek@gmail.com>
2023-09-17 17:10:13 +0200

    0119-form-inputs

--- finance-tracker/components/transaction-modal.vue
+++ finance-tracker/components/transaction-modal.vue
@@ -5,12 +5,34 @@
         Add Transaction
       </template>
 
-      <div>Hello!</div>
+      <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
+        <USelect placeholder="Select the transaction type" :options="types" />
+      </UFormGroup>
+
+      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+        <UInput type="number" placeholder="Amount" />
+      </UFormGroup>
+
+      <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
+        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
+      </UFormGroup>
+
+      <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
+        <UInput placeholder="Description" />
+      </UFormGroup>
+
+      <UFormGroup :required="true" label="Category" name="category" class="mb-4">
+        <USelect placeholder="Category" :options="categories" />
+      </UFormGroup>
+
+      <UButton type="submit" color="black" variant="solid" label="Save" />
     </UCard>
   </UModal>
 </template>
 
 <script setup>
+import { categories, types } from '~/constants'
+
 const props = defineProps({
   modelValue: Boolean
 })
--- finance-tracker/constants.js
+++ finance-tracker/constants.js
@@ -1,1 +1,3 @@
 export const transactionViewOptions = ['Yearly', 'Monthly', 'Daily']
+export const categories = ['Food', 'Housing', 'Car', 'Entertainment']
+export const types = ['Income', 'Expense', 'Saving', 'Investment']
