cf5c96481ed689eeba6d84d947d08997e7528d13
Piotr Jura <jura.piotrek@gmail.com>
2023-09-17 19:21:47 +0200

    0120-form-state

--- finance-tracker/components/transaction-modal.vue
+++ finance-tracker/components/transaction-modal.vue
@@ -5,27 +5,29 @@
         Add Transaction
       </template>
 
-      <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
-        <USelect placeholder="Select the transaction type" :options="types" />
-      </UFormGroup>
+      <UForm :state="state">
+        <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
+          <USelect placeholder="Select the transaction type" :options="types" v-model="state.type" />
+        </UFormGroup>
 
-      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-        <UInput type="number" placeholder="Amount" />
-      </UFormGroup>
+        <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+          <UInput type="number" placeholder="Amount" v-model.number="state.amount" />
+        </UFormGroup>
 
-      <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
-        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
-      </UFormGroup>
+        <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
+          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
+        </UFormGroup>
 
-      <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
-        <UInput placeholder="Description" />
-      </UFormGroup>
+        <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
+          <UInput placeholder="Description" v-model="state.description" />
+        </UFormGroup>
 
-      <UFormGroup :required="true" label="Category" name="category" class="mb-4">
-        <USelect placeholder="Category" :options="categories" />
-      </UFormGroup>
+        <UFormGroup :required="true" label="Category" name="category" class="mb-4">
+          <USelect placeholder="Category" :options="categories" v-model="state.category" />
+        </UFormGroup>
 
-      <UButton type="submit" color="black" variant="solid" label="Save" />
+        <UButton type="submit" color="black" variant="solid" label="Save" />
+      </UForm>
     </UCard>
   </UModal>
 </template>
@@ -37,6 +39,14 @@ const props = defineProps({
   modelValue: Boolean
 })
 const emit = defineEmits(['update:modelValue'])
+
+const state = ref({
+  type: undefined,
+  amount: 0,
+  created_at: undefined,
+  description: undefined,
+  category: undefined
+})
 
 const isOpen = computed({
   get: () => props.modelValue,
