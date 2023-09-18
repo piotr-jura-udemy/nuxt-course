8bb0bf19a979d861c01209f3f0e6811684b57842
Piotr Jura <jura.piotrek@gmail.com>
2023-09-18 21:05:44 +0200

    0122-zod-validation

--- finance-tracker/components/transaction-modal.vue
+++ finance-tracker/components/transaction-modal.vue
@@ -5,7 +5,7 @@
         Add Transaction
       </template>
 
-      <UForm :state="state">
+      <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
         <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
           <USelect placeholder="Select the transaction type" :options="types" v-model="state.type" />
         </UFormGroup>
@@ -22,7 +22,7 @@
           <UInput placeholder="Description" v-model="state.description" />
         </UFormGroup>
 
-        <UFormGroup :required="true" label="Category" name="category" class="mb-4">
+        <UFormGroup :required="true" label="Category" name="category" class="mb-4" v-if="state.type === 'Expense'">
           <USelect placeholder="Category" :options="categories" v-model="state.category" />
         </UFormGroup>
 
@@ -34,11 +34,43 @@
 
 <script setup>
 import { categories, types } from '~/constants'
+import { z } from 'zod'
 
 const props = defineProps({
   modelValue: Boolean
 })
 const emit = defineEmits(['update:modelValue'])
+
+const defaultSchema = z.object({
+  created_at: z.string(),
+  description: z.string().optional(),
+  amount: z.number().positive('Amount needs to be more than 0')
+})
+
+const incomeSchema = z.object({
+  type: z.literal('Income')
+})
+const expenseSchema = z.object({
+  type: z.literal('Expense'),
+  category: z.enum(categories)
+})
+const investmentSchema = z.object({
+  type: z.literal('Investment')
+})
+const savingSchema = z.object({
+  type: z.literal('Saving')
+})
+
+const schema = z.intersection(
+  z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
+  defaultSchema
+)
+
+const form = ref()
+
+const save = async () => {
+  form.value.validate()
+}
 
 const state = ref({
   type: undefined,
