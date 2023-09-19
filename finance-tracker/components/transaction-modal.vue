61ca9f109fe2e44fba99bac08000bfd49376c501
Piotr Jura <jura.piotrek@gmail.com>
2023-09-19 18:32:17 +0200

    0123-clearing-form

--- finance-tracker/components/transaction-modal.vue
+++ finance-tracker/components/transaction-modal.vue
@@ -69,19 +69,31 @@ const schema = z.intersection(
 const form = ref()
 
 const save = async () => {
-  form.value.validate()
+  if (form.value.errors.length) return
+
+  // Store into the supabase
 }
 
-const state = ref({
+const initialState = {
   type: undefined,
   amount: 0,
   created_at: undefined,
   description: undefined,
   category: undefined
+}
+const state = ref({
+  ...initialState
 })
+const resetForm = () => {
+  Object.assign(state.value, initialState)
+  form.value.clear()
+}
 
 const isOpen = computed({
   get: () => props.modelValue,
-  set: (value) => emit('update:modelValue', value)
+  set: (value) => {
+    if (!value) resetForm()
+    emit('update:modelValue', value)
+  }
 })
 </script>
--- finance-tracker/nuxt.config.ts
+++ finance-tracker/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/ui', '@nuxtjs/supabase'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   supabase: {
     redirect: false
   }
