62c16cf31c64a4de24c708c050c4ebad65b93948
Piotr Jura <jura.piotrek@gmail.com>
2023-09-25 19:36:30 +0200

    0132-sign-in-form

---
+++ finance-tracker/pages/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <UCard v-if="!success">
+    <template #header>
+      Sign-in to Finance Tracker
+    </template>
+
+    <form>
+      <UFormGroup label="Email" name="email" class="mb-4" :required="true"
+        help="You will receive an email with the confirmation link">
+        <UInput type="email" placeholder="Email" required />
+      </UFormGroup>
+
+      <UButton type="submit" variant="solid" color="black" @click="success = true">Sign-in</UButton>
+    </form>
+  </UCard>
+  <UCard v-else>
+    <template #header>
+      Email has been sent
+    </template>
+
+    <div class="text-center">
+      <p class="mb-4">We have sent an email to <strong>rmsil@email.com</strong> with a link to sign-in.</p>
+      <p>
+        <strong>Important:</strong> The link will expire in 5 minutes.
+      </p>
+    </div>
+  </UCard>
+</template>
+
+<script setup>
+const success = ref(false)
+</script>
