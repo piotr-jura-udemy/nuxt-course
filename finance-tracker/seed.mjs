c1e82fae21c294a557f9a6784d1d2e1df8c1d0de
Piotr Jura <jura.piotrek@gmail.com>
2023-09-27 17:59:29 +0200

    0138-admin-actions-seeder

--- finance-tracker/seed.mjs
+++ finance-tracker/seed.mjs
@@ -4,10 +4,13 @@ import 'dotenv/config'
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY, {
+  'INSERT_YOUR_SERVICE_KEY_HERE!', {
   auth: { persistSession: false }
 })
 const categories = ['Food', 'Housing', 'Car', 'Entertainment']
+
+const { data: { users }, error } = await supabase.auth.admin.listUsers()
+const userIds = users.map(user => user.id)
 
 async function seedTransactions() {
   // Delete existing data
@@ -21,49 +24,52 @@ async function seedTransactions() {
 
   let transactions = []
 
-  for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
-    for (let i = 0; i < 10; i++) {
-      const date = new Date(
-        year,
-        faker.number.int({ min: 0, max: 11 }),
-        faker.number.int({ min: 1, max: 28 })
-      )
+  for (const user of userIds) {
+    for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
+      for (let i = 0; i < 10; i++) {
+        const date = new Date(
+          year,
+          faker.number.int({ min: 0, max: 11 }),
+          faker.number.int({ min: 1, max: 28 })
+        )
 
-      let type, category
-      const typeBias = Math.random()
+        let type, category
+        const typeBias = Math.random()
 
-      if (typeBias < 0.85) {
-        type = 'Expense'
-        category = faker.helpers.arrayElement(categories) // Category only for 'Expense'
-      } else if (typeBias < 0.95) {
-        type = 'Income'
-      } else {
-        type = faker.helpers.arrayElement(['Saving', 'Investment'])
+        if (typeBias < 0.85) {
+          type = 'Expense'
+          category = faker.helpers.arrayElement(categories) // Category only for 'Expense'
+        } else if (typeBias < 0.95) {
+          type = 'Income'
+        } else {
+          type = faker.helpers.arrayElement(['Saving', 'Investment'])
+        }
+
+        let amount
+        switch (type) {
+          case 'Income':
+            amount = faker.number.int({ min: 2000, max: 5000 })
+            break
+          case 'Expense':
+            amount = faker.number.int({ min: 100, max: 1000 })
+            break
+          case 'Saving':
+          case 'Investment':
+            amount = faker.number.int({ min: 5000, max: 10000 })
+            break
+          default:
+            amount = 0
+        }
+
+        transactions.push({
+          created_at: date,
+          amount,
+          type,
+          description: faker.lorem.sentence(),
+          category: type === 'Expense' ? category : null, // Category only for 'Expense'
+          user_id: user
+        })
       }
-
-      let amount
-      switch (type) {
-        case 'Income':
-          amount = faker.number.int({ min: 2000, max: 5000 })
-          break
-        case 'Expense':
-          amount = faker.number.int({ min: 100, max: 1000 })
-          break
-        case 'Saving':
-        case 'Investment':
-          amount = faker.number.int({ min: 5000, max: 10000 })
-          break
-        default:
-          amount = 0
-      }
-
-      transactions.push({
-        created_at: date,
-        amount,
-        type,
-        description: faker.lorem.sentence(),
-        category: type === 'Expense' ? category : null // Category only for 'Expense'
-      })
     }
   }
 
