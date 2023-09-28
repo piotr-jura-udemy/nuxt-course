21cafa55e02db0baf094bd9c1894e05c5fc71e49
Piotr Jura <jura.piotrek@gmail.com>
2023-09-28 18:34:34 +0200

    0141-fixing-issues-rls-for-delete

--- finance-tracker/components/app-header.vue
+++ finance-tracker/components/app-header.vue
@@ -13,7 +13,7 @@
               Signed in as
             </p>
             <p class="font-medium text-gray-900 dark:text-white">
-              {{ item.label }}
+              {{ user.email }}
             </p>
           </div>
         </template>
@@ -33,7 +33,6 @@ const supabase = useSupabaseClient()
 const user = useSupabaseUser()
 const items = [
   [{
-    label: user.value?.email,
     slot: 'account',
     disabled: true
   }], [{
