8c4db8ab3775718aa4ed56a7d4951011b504729d
Piotr Jura <jura.piotrek@gmail.com>
2023-10-05 19:28:31 +0200

    0144-user-profile-saving

--- finance-tracker/pages/settings/profile.vue
+++ finance-tracker/pages/settings/profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <UForm :state="state" :schema="schema">
+  <UForm :state="state" :schema="schema" @submit.prevent="saveProfile">
     <UFormGroup class="mb-4" label="Full Name" name="name">
       <UInput v-model="state.name" />
     </UFormGroup>
@@ -9,7 +9,7 @@
       <UInput v-model="state.email" />
     </UFormGroup>
 
-    <UButton type="submit" color="black" variant="solid" label="Save" :pending="pending" />
+    <UButton type="submit" color="black" variant="solid" label="Save" :loading="pending" :disabled="pending" />
   </UForm>
 </template>
 
@@ -23,7 +23,7 @@ const { toastSuccess, toastError } = useAppToast()
 const pending = ref(false)
 
 const state = ref({
-  name: '',
+  name: user.value.user_metadata?.full_name,
   email: user.value.email
 })
 
@@ -31,4 +31,37 @@ const schema = z.object({
   name: z.string().min(2).optional(),
   email: z.string().email()
 })
+
+const saveProfile = async () => {
+  pending.value = true
+
+  try {
+    const data = {
+      data: {
+        full_name: state.value.name
+      }
+    }
+
+    if (state.value.email !== user.value.email) {
+      data.email = state.value.email
+    }
+
+    console.log(data)
+
+    const { error } = await supabase.auth.updateUser(data)
+    if (error) throw error
+
+    toastSuccess({
+      title: 'Profile updated',
+      description: 'Your profile has been updated'
+    })
+  } catch (error) {
+    toastError({
+      title: 'Error updating profile',
+      description: error.message
+    })
+  } finally {
+    pending.value = false
+  }
+}
 </script>
